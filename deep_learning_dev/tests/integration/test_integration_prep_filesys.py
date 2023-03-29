f291a5f14f211ab0c38a506935a0135e8b13cc52
Théo <48034060+theo-boi@users.noreply.github.com>
2023-03-30 00:48:07 +0200

    Added: Comments to integration tests

--- deep_learning_dev/tests/integration/test_integration_prep_filesys.py
+++ deep_learning_dev/tests/integration/test_integration_prep_filesys.py
@@ -1,42 +1,46 @@
-import os, sys, glob, logging, pytest, shutil
+import os, shutil, pytest
 
-
-# Neutralise the loggers of the data.preparation package
-logging.getLogger('deep_learning_lab.data_preparation').disabled = True
-
-    
 from deep_learning_lab.data_preparation import orchestration, patch
 
 
-test_workdir = "tests/integration/"
+test_workdir = "tests/integration/" # path to the directory where integration tests will be run
 
-orchestration.RAW_DATA_DIR = test_workdir
-patch.RESULT_DIR = os.path.join(test_workdir, "results")
+orchestration.RAW_DATA_DIR = test_workdir # path to the directory containing the raw data used for testing
+patch.RESULT_DIR = os.path.join(test_workdir, "results") # path to the directory where the results of the testing will be stored
 
-set_labels = ['TextLine']
+set_labels = ['TextLine'] # list of strings containing the labels that will be used in the testing
 
-result_dir = '_'.join(set_labels)
-result_dir_path = os.path.join(patch.RESULT_DIR, '_'.join(set_labels), "test_data")
-raw_data_dir = os.path.join(test_workdir,"raw_page_dataset")
+result_dir = '_'.join(set_labels) # name of the directory where the test results will be stored
+result_dir_path = os.path.join(patch.RESULT_DIR, '_'.join(set_labels), "test_data") # path to the directory where the test results will be stored
+raw_data_dir = os.path.join(test_workdir,"raw_page_dataset") # path to the directory containing the raw data used for testing
 
 
 @pytest.fixture(scope='session', autouse= True)
 def setup_session():
-    """Sets up the test directory by testing if the required data has already been manually downloaded.
+    """Set up the integration test directory by checking if the required data has been downloaded manually.
     
-    Expected structure of the file system:
+    The expected file system structure is as follows:
+    
     integration/            (=test_workdir/)
     └── raw_page_dataset/   (=raw_data_dir)
         ├── "image".jpg
         ├── ...
         ├── "image".xml
         └── ...
+        
+    Raises:
+        AssertionError: If the required data is not present. Run `download_test_data.sh`.
     
+    This fixture is responsible for cleaning up the result directory after the tests have run.
+
     """
+    
+    # Check if the required data files exist
     raw_data = os.listdir(raw_data_dir)
     assert "M_Aigen_am_Inn_002-01_0000.jpg" in raw_data and "M_Aigen_am_Inn_002-01_0000.xml" in raw_data, "Run `download_test_data.sh`"
     
-    for root, dirs, files in os.walk(patch.RESULT_DIR, topdown= False):
+    # Clean the result directory
+    for root, dirs, _ in os.walk(patch.RESULT_DIR, topdown= False):
         for name in dirs:
             dir_path = os.path.join(root, name)
             shutil.rmtree(dir_path)
@@ -46,12 +50,14 @@ def setup_session():
     yield
     
     # All the code bellow will be executed at the end of the tests
+    
+    pass
 
 
 def test_orch_filesystem():
-    """Patches a minimal dataset and tests whether the file system has been correctly altered.
-    
-    Expected structure of the file system:
+    """Test whether the file system has been correctly altered by patching a minimal dataset.
+
+    The expected file system structure is as follows:
     integration/            (=test_workdir/)
     ├── raw_page_dataset/   (=raw_data_dir)
     |   ├── "image".jpg
@@ -65,10 +71,13 @@ def test_orch_filesystem():
             ├── labels/
             │   └── "image".png
             └── classfile.json
+
+    Raises:
+        AssertionError: If the file system has not been correctly altered.
     
     """
     
-    # Patches
+    # Patch the dataset
     orch = orchestration.Orchestrator(output_structure= {'dir_data': "test_data",
                                            'dir_images': "images",
                                            'dir_labels': "labels"})
@@ -80,11 +89,11 @@ def test_orch_filesystem():
     orch.validate(auto_yes= True, verbose= False)
     orch.preprocess()
     
-    # Initializes assertions
+    # Initialize assertions
     is_result_dir = is_images_dir =  is_labels_dir = is_class_file = is_copied_image = is_mask = False
     original_image = os.listdir(raw_data_dir)[0]
     
-    # Tests assertions
+    # Check that the file system has been correctly altered
     if os.path.isdir(result_dir_path):
         is_result_dir = True
 
@@ -93,7 +102,7 @@ def test_orch_filesystem():
     if os.path.isdir(os.path.join(result_dir_path, "labels")):
         is_labels_dir = True
     
-    for path, currentDirectory, files in os.walk(test_workdir):
+    for path, _, files in os.walk(test_workdir):
         for file in files:
             filepath = os.path.join(path, file)
             if filepath == os.path.join(path, "classfile.json"):
@@ -102,5 +111,7 @@ def test_orch_filesystem():
                 is_copied_image = True
             if file == original_image[:original_image.find('.jpg')]+'.png':
                 is_mask = True
+
+    # Assert that all checks pass
     bools = (is_result_dir, is_images_dir, is_labels_dir, is_class_file, is_copied_image, is_mask)
     assert all(bools), "File system has not correctly been altered"
--- deep_learning_dev/tests/integration/test_integration_train_infer.py
+++ deep_learning_dev/tests/integration/test_integration_train_infer.py
@@ -1,39 +1,62 @@
-import os, sys, glob, logging, pytest, shutil
-
-
-# Neutralise the loggers of the data.preparation package
-logging.getLogger('deep_learning_lab.data_preparation').disabled = True
-
+import os, glob, shutil, pytest
 
 from deep_learning_lab import model
 from deep_learning_lab.data_preparation import orchestration, patch
 
 
-test_workdir = "tests/integration/"
+test_workdir = "tests/integration/" # path to the directory where integration tests will be run
 
-orchestration.RAW_DATA_DIR = test_workdir
-patch.RESULT_DIR = os.path.join(test_workdir, "results")
+orchestration.RAW_DATA_DIR = test_workdir # path to the directory containing the raw data used for testing
+patch.RESULT_DIR = os.path.join(test_workdir, "results") # path to the directory where the results of the testing will be stored
 
-set_labels = ['TextLine']
+set_labels = ['TextLine'] # list of strings containing the labels that will be used in the testing
 
-labels_dir = '_'.join(set_labels)
-labels_dir_path = os.path.join(patch.RESULT_DIR, '_'.join(set_labels))
-dataset_path = os.path.join(labels_dir_path, "test_data")
+labels_dir = '_'.join(set_labels) # name of the directory of test results specified by the set of labels
+labels_dir_path = os.path.join(patch.RESULT_DIR, '_'.join(set_labels))# path to the directory of test results specified by the set of labels
+dataset_path = os.path.join(labels_dir_path, "test_data") # path of the directory containing the test data used in the testing
 
 
 @pytest.fixture(scope='session', autouse=True)
 def setup_session():
-    # Initializes assertions
+    """Sets up the session fixture for the integration test.
+
+    The function initializes and tests the assertions, and cleans up the labels directory.
+    The assertions check if the directory path for the labels exists, and raise a warning if test_integration_prep_files
+    is not executed prior to running the tests. The labels directory is then cleaned up using shutil.rmtree.
+
+    The directory structure of the file system is expected to be like this:
+
+    integration/           (=test_workdir)
+    └── results/           (=patch.RESULT_DIR)
+        └── TextLine/      (=labels_dir_path)
+            └── test_data/ (=dataset_path)
+                ├── images/
+                │   ├── image.jpg
+                |   └── ...
+                ├── labels/
+                |   ├── image.png
+                |   └── ...
+                └── classfile.json
+
+    Returns:
+        The setup function as a fixture for the integration test.
+
+    Raises:
+        AssertionError: If the directory path for the labels does not exist.
+        
+    """
+    
+    # Initialize assertions
     is_labels_dir = False
     
-    # Tests assertions
+    # Test assertions
     if os.path.isdir(labels_dir_path):
         is_labels_dir = True
         
     assert is_labels_dir, "WARNING: Please execute `test_integration_prep_files` first to patch data"
     
     # Clean the labels directory
-    for root, dirs, files in os.walk(labels_dir_path, topdown= False):
+    for root, dirs, _ in os.walk(labels_dir_path, topdown= False):
         if root != os.path.join(labels_dir_path, "test_data"):
             for name in dirs:
                 if name != "test_data":
@@ -52,9 +75,10 @@ def setup_session():
 
 @pytest.fixture(scope='session')
 def setup_train_env():
-    """Sets up the test directory by testing if the required data has already been manually downloaded.
+    """Sets up the test environment by verifying if the required data has been manually downloaded.
+
+    The function asserts the expected directory structure of the file system:
     
-    Expected structure of the file system at the end:
     integration/           (=test_workdir)
     └── results/           (=patch.RESULT_DIR)
         └── TextLine/      (=labels_dir_path)
@@ -67,23 +91,31 @@ def setup_train_env():
                 |   └── ...
                 └── classfile.json
     
+    Raises:
+        AssertionError: If any of the assertions fail, the test environment setup is incomplete.
+            - When the downloaded files are not present.
+            - When the expected directory structure is not found.
+        
     """
+
+    # Check if the required data files exist
     raw_data_dir = os.path.join(test_workdir,"raw_page_dataset")
     raw_data = os.listdir(raw_data_dir)
     assert "M_Aigen_am_Inn_002-01_0000.jpg" in raw_data and "M_Aigen_am_Inn_002-01_0000.xml" in raw_data, \
     "WARNING: Please run `download_test_data.sh`and execute `test_integration_prep_files` first to patch data"
     
-    # Initializes assertions
+    # Initialize assertions
     is_images_dir = is_labels_dir = is_class_file = is_copied_image = is_mask = False
     original_image = os.path.basename(glob.glob(os.path.join(raw_data_dir, '*.jpg'))[0])
     
-    # Tests assertions
+    # Check if images and labels directory exist
     if os.path.isdir(os.path.join(dataset_path, "images")):
         is_images_dir = True
     if os.path.isdir(os.path.join(dataset_path, "labels")):
         is_labels_dir = True
     
-    for path, currentDirectory, files in os.walk(test_workdir):
+    # Walk through the directory and check if required files exist
+    for path, _, files in os.walk(test_workdir):
         for file in files:
             filepath = os.path.join(path, file)
             if filepath == os.path.join(path, "classfile.json"):
@@ -92,14 +124,17 @@ def setup_train_env():
                 is_copied_image = True
             if file == original_image[:original_image.find('.jpg')]+'.png':
                 is_mask = True
+
+    # Check if all the required files exist
     bools = (is_images_dir, is_labels_dir, is_class_file, is_copied_image, is_mask)
     assert all(bools), "WARNING: Please execute `test_integration_prep_files` first to patch data"
 
 
 def test_training(setup_train_env):
-    """Train a dummy model.
-    
-    Expected structure of the file system at the end:
+    """Test training of a dummy model.
+
+    Asserts that a model is trained and serialized in the expected directory structure:
+
     integration/           (=test_workdir)
     └── results/           (=patch.RESULT_DIR)
         └── TextLine/      (=labels_dir_path)
@@ -117,8 +152,15 @@ def test_training(setup_train_env):
                 ├── test.csv
                 ├── train.csv
                 └── val.csv
+
+    Args:
+        setup_train_env: A fixture that sets up a training environment.
+
+    Raises:
+        AssertionError: If the model is not trained and serialized in the expected directory structure.
     
     """
+    # Instantiate a Trainer object with given parameters
     trainer = model.Trainer(
         labels= set_labels,
         input_dir= "test_data",
@@ -128,19 +170,23 @@ def test_training(setup_train_env):
         working_dir= patch.RESULT_DIR
     )
     
+    # Check that all necessary data files are present
     csv_files = glob.glob(os.path.join(dataset_path, '*.csv'))
     assert all(
         any(f+'.csv' in csv for csv in csv_files)
         for f in ('data', 'train', 'val')
     ), "Missing data files among 'data.csv', 'train.csv', 'val.csv'"
     
+    # Train the model
     trainer.train()
     
+    # Check that model files have been serialized
     model_dir = os.path.join(labels_dir_path, 'model')
     model_files = [name for name in os.listdir(model_dir) if os.path.isfile(os.path.join(model_dir, name))]
-    assert all("checkpoint" in n for n in model_files) and len(model_files), \
-    "No model has been serialized or serialized files do not contain 'checkpoint'"
+    assert any("model_checkpoint" in m for m in model_files) and len(model_files), \
+    f"No model has been serialized or serialized files. {model_dir} does not contain checkpoint"
     
+    # Check that tensorboard logs have been created
     tensor_logs = []
     log_dir = os.path.join(labels_dir_path, "tensorboard", "log")
     if os.path.isdir(log_dir):
@@ -150,30 +196,35 @@ def test_training(setup_train_env):
     
 @pytest.fixture(scope='session')
 def setup_infer_env():
-    """Sets up the test directory by testing if the required model has already been trained.
+    """Sets up the test directory and checks whether the required model has already been trained.
     
-    Expected structure of the file system at the end:
-    integration/           (=test_workdir)
-    └── results/           (=patch.RESULT_DIR)
-        └── TextLine/      (=labels_dir_path)
-            ├── model/
-            └── test_data/ (=dataset_path)
+    Directory structure at the end:
+    integration/               (test_workdir)
+    └── results/               (patch.RESULT_DIR)
+        └── TextLine/          (labels_dir_path)
+            ├── model/         (model_dir)
+            └── test_data/     (dataset_path)
                 └── images/
                     ├── image.jpg
                     └── ...
     
+    Raises:
+        AssertionError: If images directory does not exist or contains less than two images.
+        AssertionError: If no model has been serialized or serialized files are not found.
+    
     """
     
-    # Initializes assertions
+    # Initialize assertions
     is_images_dir = False
     
-    # Tests assertions
+    # Check that images directory exist and contain at least 2 images
     images_path = os.path.join(dataset_path, "images")
-    if os.path.isdir(images_path) and len(os.listdir(images_path)) == 2:
+    if os.path.isdir(images_path) and len(os.listdir(images_path)) >= 2:
         is_images_dir = True
     
     assert is_images_dir, "WARNING: Please execute `test_integration_prep_files` first to patch data"
 
+    # Check that model files have been serialized
     model_dir = os.path.join(labels_dir_path, 'model')
     model_files = [name for name in os.listdir(model_dir) if os.path.isfile(os.path.join(model_dir, name))]
     assert any("model_checkpoint" in m for m in model_files) and len(model_files), \
@@ -181,9 +232,9 @@ def setup_infer_env():
     
 
 def test_inference(setup_infer_env):
-    """Make inference using a dummy predictor.
-    
-    Expected structure of the file system at the end:
+    """Run inference using a dummy predictor.
+
+    The expected structure of the file system after running the test:
     integration/           (=test_workdir)
     └── results/           (=patch.RESULT_DIR)
         └── TextLine/      (=labels_dir_path)
@@ -196,9 +247,15 @@ def test_inference(setup_infer_env):
                     ├── image.jpg
                     └── ...
     
+    Raises:
+    AssertionError: If the output directory of the predictor or inference doesn't work
+
+    Args:
+    setup_infer_env: An instance of the test environment to run the inference
+
     """
     
-    # Makes dummy predictions
+    # Make dummy predictions
     predictor = model.Predictor(
         set_labels,
         input_dir= os.path.join("test_data", "images"),
@@ -207,7 +264,7 @@ def test_inference(setup_infer_env):
     )
     results = predictor.start(verbose= False)
     
-    # Tests assertions
+    # Test assertions
     preds_dir = os.path.join(labels_dir_path, "predictions")
     assert os.path.isdir(preds_dir), "Output of the predictor is absent"
     assert len(results) == len(os.listdir(os.path.join(dataset_path, "images"))), "Inference did not work"
